6fafdf32cdbddc30627462cb8177494a69899daa
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-01 16:54:23 -0400

    all_students possibly working? Proceed with caution

--- classes/student.py
+++ classes/student.py
@@ -1,7 +1,22 @@
-from classes.person import Person
-
+from person import Person
+import csv
+import os
+my_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(my_path, "../data/students.csv")
 class Student(Person):
+
+    @classmethod
+    def all_students(cls):
+        output_list = []
+        with open(path) as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                output_list.append(row)
+
 
     def __init__(self, name, age, role, password, school_id):
         Person.__init__(self, name, age, role, password)
         self.school_id = school_id
+
+# test_list = Student.all_students()
+# print(test_list)
