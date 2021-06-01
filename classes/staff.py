351210ec20b4c3477344b6a21dc69edf14dae46f
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-01 17:00:53 -0400

    Added all_staff() with same logic as all_student() meaning, questionable.

--- classes/staff.py
+++ classes/staff.py
@@ -1,6 +1,19 @@
-from classes.person import Person
+from person import Person
+import csv
+import os
+my_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(my_path, "../data/staff.csv")
 
 class Staff(Person):
+
+    @classmethod
+    def all_staff(cls):
+        output_list = []
+        with open(path) as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                output_list.append(row)
+
 
     def __init__(self, name, age, role, password, employee_id):
         Person.__init__(self, name, age, role, password)
