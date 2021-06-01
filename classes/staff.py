e0d32029357abd68e69f755375b09ea06ba703f2
ThomasVirn <thomas.virnston1@gmail.com>
2021-06-01 17:05:55 -0500

    school interface part one, partial solution

---
+++ classes/staff.py
@@ -0,0 +1,19 @@
+from classes.person import Person
+import csv
+import os.path
+
+
+class Staff(Person):
+    def __init__(self, name, age, role, employee_id, password):
+        super().__init__(name, age, role, password)
+        self.employee_id = employee_id
+    @classmethod
+    def all_staff():
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, "../data/staff.csv")
+        with open(path) as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for row in csv_reader:
+                print(row)
+
+        
---
+++ classes/student.py
@@ -0,0 +1,26 @@
+from classes.person import Person
+import csv
+import os.path
+
+class Student(Person):
+    def __init__(self, name, age, role, school_id, password):
+        super().__init__(name, age, role, password)
+        self.school_id = school_id
+
+    @classmethod
+    def all_students():
+        students = {}
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, "../data/students.csv")
+        with open(path) as csvfile:
+            csv_reader = csv.Dictreader(csvfile)
+            for row in csv_reader:
+                students.update(Student(**dict(row)))
+                return students
+
+
+# student_info = {'name' : 'Diana', 'password' : 'password', 'school_id' : 12345, 'age' : 17, 'role' : 'Student'}
+
+# s = Student(**student_info)
+# #s.print_student
+# s.all_students()
