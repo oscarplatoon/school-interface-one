460833100f2654049a5d594b8511ddb3e8fb4cdc
SarahShinespark <sarahshinespark@users.noreply.github.com>
2021-06-01 22:33:24 +0000

    Sarah and Tim school interface one

---
+++ classes/person.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, name, age, role, password):
+        self.name = name 
+        self.age = age         
+        self.password = password
+        self.role = role
+        
