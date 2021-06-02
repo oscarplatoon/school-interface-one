67bf4f880dae1b27711e5221199c275e58e5a284
ver2point0 <jmci10@hotmail.com>
2021-06-02 14:08:34 -0700

    John M school interface one

--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
-from school import School 
+from classes.school import School
 
-school = School('Ridgemont High') 
+school = School('Ridgemont High')
 
-print(school.name)
+print(school.staff)
+print(school.students)
