716ec6c15a8408be5acfbf5e12334d0e77a03854
awhitford10 <awhitford@Andrews-Mac-mini.local>
2021-06-02 15:30:38 -0400

    Andrew_school interface 1

--- runner.py
+++ runner.py
@@ -1,5 +1,8 @@
-from school import School 
+from classes.school import School 
+from classes.student import Student
+from classes.staff import Staff
 
 school = School('Ridgemont High') 
 
-print(school.name)
+print(school.staff)
+print(school.students)
