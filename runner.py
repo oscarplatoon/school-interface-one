709e2ab107942e67c5c4f476e14ab5d90b562e0f
vzbrunstad <vzbrunstad@msn.com>
2021-06-02 08:11:20 -0500

    Vince B School Interface One Solution

--- runner.py
+++ runner.py
@@ -1,13 +1,5 @@
 from classes.school import School 
-# from classes.person import Person
-from classes.student import Student
-from classes.staff import Staff
 
 school = School('Ridgemont High')
-# student_input = {'name': 'tom', 'age':'37', 'password': 'password', 'role': 'student'}
-# tom = Student(**student_input)
-# tom = Person(**person_input)
-Student.all_students()
 
-
-#print(school.name)
+school.display_school()
