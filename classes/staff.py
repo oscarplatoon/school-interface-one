c2f3e4efbbd5b822769851009c2cb7d5761d5211
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-02 11:00:49 -0400

    Reorganized and updated code to reflect Interface Two suggested Outputs.

--- classes/staff.py
+++ classes/staff.py
@@ -1,21 +1,21 @@
 from .person import Person
 import csv
-import os
-my_path = os.path.abspath(os.path.dirname(__file__))
-path = os.path.join(my_path, "../data/staff.csv")
+import os.path
 
 class Staff(Person):
 
+    def __init__(self, name, age, role, password, employee_id):
+        super().__init__(name, age, role, password)
+        self.employee_id = employee_id
+
     @classmethod
     def all_staff(cls):
-        output_list = []
+        staff = []
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, "../data/staff.csv")
+
         with open(path) as csv_file:
             csv_reader = csv.DictReader(csv_file)
             for row in csv_reader:
-                output_list.append(Staff(**row))
-            return output_list
-
-
-    def __init__(self, name, age, role, password, employee_id):
-        Person.__init__(self, name, age, role, password)
-        self.employee_id = employee_id
+                staff.append(Staff(**row))
+            return staff
--- classes/student.py
+++ classes/student.py
@@ -1,24 +1,22 @@
 from .person import Person
 import csv
 import os
-my_path = os.path.abspath(os.path.dirname(__file__))
-path = os.path.join(my_path, "../data/students.csv")
+
+
 class Student(Person):
+
+    def __init__(self, name, age, role, password, school_id):
+        super().__init__(name, age, role, password)
+        self.school_id = school_id
 
     @classmethod
     def all_students(cls):
-        output_list = []
+        students = []
+
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, "../data/students.csv")
         with open(path) as csv_file:
             csv_reader = csv.DictReader(csv_file)
             for row in csv_reader:
-                output_list.append(Student(**row))
-            return output_list
-
-
-
-    def __init__(self, name, age, role, password, school_id):
-        Person.__init__(self, name, age, role, password)
-        self.school_id = school_id
-
-# test_list = Student.all_students()
-Student.all_students()
+                students.append(Student(**row))
+        return students
